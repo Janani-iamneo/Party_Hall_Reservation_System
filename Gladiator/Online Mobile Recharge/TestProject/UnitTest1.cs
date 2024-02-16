d084a9cb9ee83e172aa58ee5e8a436584f137b7f
neojarvis <techadmin@iamneo.ai>
2024-02-16 00:07:10 +0000

    Auto-save-commit-16/02/2024--05:37:08

--- Gladiator/Online Mobile Recharge/TestProject/UnitTest1.cs
+++ Gladiator/Online Mobile Recharge/TestProject/UnitTest1.cs
@@ -1,15 +1,50 @@
-namespace TestProject;
+using NUnit.Framework;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-public class Tests
+[TestFixture]
+public class SpringappApplicationTests
 {
+    private HttpClient _httpClient;
+    private string _generatedToken;
+
     [SetUp]
     public void Setup()
     {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://8080-fcebdccccdbcfacbdcbaeadbebabcdebdca.premiumproject.examly.io"); 
     }
 
-    [Test]
-    public void Test1()
+    [Test, Order(1)]
+    public async Task Backend_TestRegisterUser()
     {
-        Assert.Pass();
+        string uniqueId = Guid.NewGuid().ToString();
+
+        // Generate a unique userName based on a timestamp
+        string uniqueUsername = $"abcd_{uniqueId}";
+        string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+        var registrationRequest = new
+        {
+            Username = uniqueUsername,
+            Password = "abc@123A",
+            Email = uniqueEmail,
+            MobileNumber = "1234567890",
+            Role = "customer" // or "admin" based on your requirements
+        };
+
+        string requestBody = JsonConvert.SerializeObject(registrationRequest);
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+        Console.WriteLine(response.StatusCode);
+        string responseString = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseString);
+
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
     }
 }
