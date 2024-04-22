bf44d2f6a3997085812637b2c7ea37910b784c04
neojarvis <techadmin@iamneo.ai>
2024-04-22 11:16:46 +0000

    Auto-save-commit-22/04/2024--16:46:44

--- Recipe_MS3-main/Receipe/angularapp/src/app/services/booking.service.spec.ts
+++ Recipe_MS3-main/Receipe/angularapp/src/app/services/booking.service.spec.ts
@@ -1,16 +1,76 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { Booking } from '../models/booking.model';
 import { BookingService } from './booking.service';
 
 describe('BookingService', () => {
   let service: BookingService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BookingService],
+    });
     service = TestBed.inject(BookingService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  fit('BookingService_should_be_created', () => {
     expect(service).toBeTruthy();
   });
+
+  fit('BookingService_should_have_addBooking_method', () => {
+    expect(service.addBooking).toBeTruthy();
+  });
+
+  fit('BookingService_should_have_getRecipes_method', () => {
+    expect(service.getRecipes).toBeTruthy();
+  });
+
+  // fit('RecipeService_should_add_a_recipe_and_return_it', () => {
+  //   const mockRecipe: Recipe = {
+  //     recipeId: 1,
+  //     name: 'Test Recipe',
+  //     description: 'Test Description',
+  //     ingredients: 'Test Ingredients',
+  //     instructions: 'Test Instructions',
+  //     author: 'Test Author'
+  //   };
+
+  //   service.addRecipe(mockRecipe).subscribe((recipe) => {
+  //     expect(recipe).toEqual(mockRecipe);
+  //   });
+
+  //   const req = httpTestingController.expectOne(`${service['apiUrl']}api/Recipe`);
+  //   expect(req.request.method).toBe('POST');
+  //   req.flush(mockRecipe);
+  // });
+
+  // fit('RecipeService_should_get_recipes', () => {
+  //   const mockRecipes: Recipe[] = [
+  //     {
+  //       recipeId: 1,
+  //       name: 'Test Recipe 1',
+  //       description: 'Test Description',
+  //       ingredients: 'Test Ingredients',
+  //       instructions: 'Test Instructions',
+  //       author: 'Test Author'
+  //     }
+  //   ];
+
+  //   service.getRecipes().subscribe((recipes) => {
+  //     expect(recipes).toEqual(mockRecipes);
+  //   });
+
+  //   const req = httpTestingController.expectOne(`${service['apiUrl']}api/Recipe`);
+  //   expect(req.request.method).toBe('GET');
+  //   req.flush(mockRecipes);
+  // });
 });
+
