6863697fdb4329087385b03827c8fb9cda6f52e4
neojarvis <techadmin@iamneo.ai>
2024-04-22 10:31:46 +0000

    Auto-save-commit-22/04/2024--16:01:44

--- Recipe_MS3-main/Receipe/angularapp/src/app/header/header.component.spec.ts
+++ Recipe_MS3-main/Receipe/angularapp/src/app/header/header.component.spec.ts
@@ -1,25 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
+    let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HeaderComponent],
+            imports: [RouterTestingModule],
+        }).compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        router = TestBed.inject(Router);
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    fit('should_create_HeaderComponent', () => {
+        expect(component).toBeTruthy();
+    });
+
+    fit('HeaderComponent_should_navigate_to_Add_New_Booking', () => {
+        spyOn(router, 'navigate');
+        component.navigateToAddBooking();
+        expect(router.navigate).toHaveBeenCalledWith(['/addNewBooking']);
+    });
+
+    fit('HeaderComponent_should_navigate_to_View_Booking', () => {
+        spyOn(router, 'navigate');
+        component.navigateToViewBookings();
+        expect(router.navigate).toHaveBeenCalledWith(['/viewBookings']);
+    });
 });
