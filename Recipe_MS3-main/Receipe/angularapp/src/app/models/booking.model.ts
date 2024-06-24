e40df7196b690a3d1fdb37c3b56e30e3fb6a0e3d
neojarvis <techadmin@iamneo.ai>
2024-06-24 06:38:20 +0000

    Auto-save-commit-24/06/2024--12:08:20

--- Recipe_MS3-main/Receipe/angularapp/src/app/models/booking.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Booking {
-    bookingId: number;
-    customerName: string;
-    bookingDate: string;
-    numberOfGuests: number;
-    contactNumber: string;
-    email: string;
-    specialRequests: string;
-    }
